37214d8f8e15c750503300e2a44de78c8548e8a0
mmjck <msantosjck@gmail.com>
2024-03-11 23:32:52 -0400

    create delivery

---
+++ src/modules/deliveries/use_cases/create_delivery/create_delivery_controller.ts
@@ -0,0 +1,13 @@
+import { CreateDeliveryUseCase } from "./create_delivery_use_case";
+import { Request, Response } from "express";
+
+export class CreateDeliveryController {
+    async handle(request: Request, response: Response) {
+        const useCase = new CreateDeliveryUseCase()
+        const { id_client, item_name } = request.body
+
+
+        const delivery = await useCase.execute({ id_client, item_name })
+        return response.json(delivery)
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from "./modules/clients/use_cases/create_clien
 import { AuthenticateClientController } from "./modules/account/authenticate_client/authenticate_client_controller";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticate_deliveryman/authenticate_deliveryman_controller";
 import { CreateDeliverymanController } from "./modules/deliveryman/usecases/create_deliveryman/create_deliveryman_controller";
+import { CreateDeliveryController } from "./modules/deliveries/use_cases/create_delivery/create_delivery_controller";
 
 const routes = Router()
 
@@ -11,7 +12,7 @@ const authenticateDeliveryManController = new AuthenticateDeliverymanController(
 
 const createClientController = new CreateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
-
+const deliveryConroller = new CreateDeliveryController()
 
 routes.get("/", (res, resp) => resp.json("ola"))
 
@@ -25,5 +26,6 @@ routes.post("/client", createClientController.handle)
 
 routes.post("/deliveryman", createDeliverymanController.handle)
 
+routes.post("/delivery", deliveryConroller.handle)
 
 export { routes }
