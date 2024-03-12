c146008e7396af075dbe825fab72b1f47e44b070
mmjck <msantosjck@gmail.com>
2024-03-11 22:49:37 -0400

    added new handle

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,29 @@
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/use_cases/create_client/create_client_controller";
 import { AuthenticateClientController } from "./modules/account/authenticate_client/authenticate_client_controller";
-import { CreateDeliveryManController } from "./modules/deliveryman/usecases/create_deliveryman/create_deliveryman_controller";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticate_deliveryman/authenticate_deliveryman_controller";
+import { CreateDeliverymanController } from "./modules/deliveryman/usecases/create_deliveryman/create_deliveryman_controller";
 
 const routes = Router()
-const createClientController = new CreateClientController()
+
 const authenticateClientController = new AuthenticateClientController()
-const createDeliveryManController = new CreateDeliveryManController()
+const authenticateDeliveryManController = new AuthenticateDeliverymanController()
+
+const createClientController = new CreateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
 
 
 routes.get("/", (res, resp) => resp.json("ola"))
 
 
 
-routes.post("/authenticate", authenticateClientController.handle)
+routes.post("/client/authenticate", authenticateClientController.handle)
+routes.post("/deliveryman/authenticate", authenticateDeliveryManController.handle)
+
+
 routes.post("/client", createClientController.handle)
 
-routes.post("/deliveryman", createDeliveryManController.handle)
+routes.post("/deliveryman", createDeliverymanController.handle)
 
 
 export { routes }
