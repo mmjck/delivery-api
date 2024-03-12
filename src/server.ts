5b709377d695d6858dd48ba75c67aec2092b4c75
mmjck <msantosjck@gmail.com>
2024-03-11 22:31:54 -0400

    handle errors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
-import  express  from "express";
-
-
+import express, { NextFunction, Request, Response }  from "express";
+import { routes } from "./routes";
+import "express-async-errors"
 const app = express()
+app.use(express.json())
+app.use(routes)
 
 
-app.get("/", (reqt, res) => {
-    return res.json({
-        message: "Hello world"
+app.use((err: Error, req: Request, resp: Response, next: NextFunction) => {
+
+    if(err instanceof Error){
+        return resp.status(400).json({
+            message: err.message
+        })
+    }
+
+    return resp.status(400).json({
+        status:"error",
+        message: "Internal server error"
     })
 })
-
 app.listen(3000, () => {
     console.log("Server is running ")
 })
